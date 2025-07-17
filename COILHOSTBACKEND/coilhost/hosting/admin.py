c3b87310cee9b3f0f74b2e57bce356c35fcc42e6
Danil <deltayt.ro@gmail.com>
2025-07-17 23:52:07 +0300

    Update admin.py

--- COILHOSTBACKEND/coilhost/hosting/admin.py
+++ COILHOSTBACKEND/coilhost/hosting/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Tariff
+from .models import Tariff, Category
 
 @admin.register(Tariff)
 class TariffAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "region")
+    list_display = ("name", "price", "region", "ethernet", "ddos")
     search_fields = ("name", "region", "cpu", "ram", "storage")
     list_filter = ("region",)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    search_fields = ("name", "slug")
